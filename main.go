7fba847871b8bc0e8b66bf8a622435ebac51342b
daltondiz <dalton.dias@ymail.com>
2019-11-07 23:34:05 -0300

    URL routing with httprouter

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 
-	"github.com/russross/blackfriday"
+	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
@@ -13,12 +14,53 @@ func main() {
 		port = "8080"
 	}
 
-	http.HandleFunc("/markdown", GenerateMarkdown)
-	http.Handle("/", http.FileServer(http.Dir(".")))
-	http.ListenAndServe(":"+port, nil)
+	router := httprouter.New()
+	router.GET("/", HomeHandler)
+
+	router.GET("/posts", PostsIndexHandler)
+	router.POST("/posts", PostsCreateHandler)
+
+	router.GET("/posts/:id", PostShowHandler)
+	router.PUT("/posts/:id", PostUpdateHandler)
+	router.GET("/posts/:id/edit", PostEditHandler)
+
+	fmt.Println("Starting server on: " + port)
+	router.NotFound = http.FileServer(http.Dir("public"))
+	http.ListenAndServe(":"+port, router)
 }
 
-func GenerateMarkdown(rw http.ResponseWriter, r *http.Request) {
-	markdown := blackfriday.MarkdownCommon([]byte(r.FormValue("body")))
-	rw.Write(markdown)
+// HomeHandler Redirect to home
+func HomeHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	fmt.Fprintln(rw, "Home")
+}
+
+// PostsIndexHandler posts index
+func PostsIndexHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	fmt.Fprintln(rw, "posts index")
+}
+
+// PostsCreateHandler create new post
+func PostsCreateHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	fmt.Fprintln(rw, "posts create")
+}
+
+// PostShowHandler show post by id
+func PostShowHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	id := p.ByName("id")
+	fmt.Fprintln(rw, "showing post", id)
+}
+
+// PostUpdateHandler Update post
+func PostUpdateHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	fmt.Fprintln(rw, "post update")
+}
+
+// PostDeleteHandler Delete post
+func PostDeleteHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	fmt.Fprintln(rw, "post delete")
+}
+
+// PostEditHandler Edit post
+func PostEditHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	fmt.Fprintln(rw, "post edit")
 }
