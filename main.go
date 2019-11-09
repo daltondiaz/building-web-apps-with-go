e799d3c88e1efa75e08ea4890f48ac17e6dee964
daltondiz <dalton.dias@ymail.com>
2019-11-08 23:30:35 -0300

    Middleware

--- main.go
+++ main.go
@@ -1,66 +1,34 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
-	"os"
 
-	"github.com/julienschmidt/httprouter"
+	"github.com/codegangsta/negroni"
 )
 
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
+
+	// Middleware stack
+	n := negroni.New(
+		negroni.NewRecovery(),
+		negroni.HandlerFunc(MyFirstGoMiddleware),
+		negroni.NewLogger(),
+		negroni.NewStatic(http.Dir("public")),
+	)
+
+	n.Run(":8080")
+}
+
+// MyFirstGoMiddleware this is my first Middleware make in Go Lang
+func MyFirstGoMiddleware(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	log.Println("Logging on the way there...")
+
+	if r.URL.Query().Get("password") == "123" {
+		next(rw, r)
+	} else {
+		http.Error(rw, "Not Authorized", 401)
 	}
 
-	router := httprouter.New()
-	router.GET("/", HomeHandler)
-
-	router.GET("/posts", PostsIndexHandler)
-	router.POST("/posts", PostsCreateHandler)
-
-	router.GET("/posts/:id", PostShowHandler)
-	router.PUT("/posts/:id", PostUpdateHandler)
-	router.GET("/posts/:id/edit", PostEditHandler)
-
-	fmt.Println("Starting server on: " + port)
-	router.NotFound = http.FileServer(http.Dir("public"))
-	http.ListenAndServe(":"+port, router)
-}
-
-// HomeHandler Redirect to home
-func HomeHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	fmt.Fprintln(rw, "Home")
-}
-
-// PostsIndexHandler posts index
-func PostsIndexHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	fmt.Fprintln(rw, "posts index")
-}
-
-// PostsCreateHandler create new post
-func PostsCreateHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	fmt.Fprintln(rw, "posts create")
-}
-
-// PostShowHandler show post by id
-func PostShowHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	id := p.ByName("id")
-	fmt.Fprintln(rw, "showing post", id)
-}
-
-// PostUpdateHandler Update post
-func PostUpdateHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	fmt.Fprintln(rw, "post update")
-}
-
-// PostDeleteHandler Delete post
-func PostDeleteHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	fmt.Fprintln(rw, "post delete")
-}
-
-// PostEditHandler Edit post
-func PostEditHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	fmt.Fprintln(rw, "post edit")
+	log.Println("Logging on the way back...")
 }
